1072b14d3518e7b149c3cf34f3e66fd8003cd2e7
sanketpanasuriya <pansuriyasanket555@gmail.com>
2024-08-06 19:07:23 +0530

    added active admin setup

--- db/migrate/20240806125747_devise_create_admin_users.rb
+++ db/migrate/20240806125747_devise_create_admin_users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DeviseCreateAdminUsers < ActiveRecord::Migration[7.1]
-  def change
+  def self.up
     create_table :admin_users do |t|
       ## Database authenticatable
       t.string :email,              null: false, default: ""
@@ -32,8 +32,8 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration[7.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
-      t.timestamps null: false
+      # Uncomment below if timestamps were not included in your original model.
+      # t.timestamps null: false
     end
 
     add_index :admin_users, :email,                unique: true
@@ -41,4 +41,10 @@ class DeviseCreateAdminUsers < ActiveRecord::Migration[7.1]
     # add_index :admin_users, :confirmation_token,   unique: true
     # add_index :admin_users, :unlock_token,         unique: true
   end
+
+  def self.down
+    # By default, we don't want to make any assumption about how to roll back a migration when your
+    # model already existed. Please edit below which fields you would like to remove in this migration.
+    raise ActiveRecord::IrreversibleMigration
+  end  
 end
